a5f85e8889dd30fd6cb9f21a3751cec9a8cf55fd
Josh Jones <jonesjo7@oregonstate.edu>
2020-10-06 21:17:38 -0700

    Edge Behavior classes created

--- models/simulations/langtons_ant/EdgeBehavior.cpp
+++ models/simulations/langtons_ant/EdgeBehavior.cpp
@@ -3,3 +3,104 @@
 //
 
 #include "EdgeBehavior.hpp"
+
+std::pair<int, int> EdgeBehavior::adjustPos(std::pair<int, int> nextPos) {
+    if(nextPos.first < 0){
+        nextPos.first = 0;
+    } else if (nextPos.first >= LEDBoard::PIXEL_ROWS){
+        nextPos.first = LEDBoard::PIXEL_ROWS - 1;
+    }
+
+    if(nextPos.second < 0){
+        nextPos.second = 0;
+    } else if (nextPos.second >= LEDBoard::PIXEL_COLS) {
+        nextPos.second = LEDBoard::PIXEL_COLS - 1;
+    }
+
+    return nextPos;
+}
+
+bool EdgeBehavior::isEdge(std::pair<int, int> curPos, Direction direction) {
+    std::pair<int, int> nextPos = moveBehavior->move(curPos, direction);
+    if(nextPos.first < 0 || nextPos.second < 0){
+        return true;
+    }
+
+    if(nextPos.first > LEDBoard::PIXEL_ROWS || nextPos.second > LEDBoard::PIXEL_COLS){
+        return true;
+    }
+
+    return false;
+}
+
+void EdgeBehavior::setMoveBehavior(MoveBehavior *moveBx) {
+    this->moveBehavior = moveBx;
+}
+
+void EdgeBehavior::setTurnBehavior(TurnBehavior *turnBx) {
+    this->turnBehavior = turnBx;
+}
+
+std::pair<int, int> Teleport::handleEdge(std::pair<int, int> curPos, Direction &direction, int colorVal) {
+    std::pair<int, int> nextPos = moveBehavior->move(curPos, direction);
+
+    nextPos.first += LEDBoard::PIXEL_ROWS;
+    nextPos.first %= LEDBoard::PIXEL_ROWS;
+
+    nextPos.second += LEDBoard::PIXEL_COLS;
+    nextPos.second %= LEDBoard::PIXEL_COLS;
+
+    return nextPos;
+}
+
+std::pair<int, int> UTurn::handleEdge(std::pair<int, int> curPos, Direction &direction, int colorVal) {
+    std::pair<int, int> nextPos = adjustPos(moveBehavior->move(curPos, direction));
+
+    direction = static_cast<Direction>((direction + NUM_DIRECTIONS / 2) % NUM_DIRECTIONS);
+
+    return nextPos;
+}
+
+Death::Death(bool remove) {
+    removeOnDeath = remove;
+}
+
+std::pair<int, int> Death::handleEdge(std::pair<int, int> curPos, Direction &direction, int colorVal) {
+    std::pair<int, int> nextPos = adjustPos(moveBehavior->move(curPos, direction));
+
+    if(removeOnDeath){
+        nextPos = std::make_pair(-1,-1);
+    }
+
+    moveBehavior->setMoveDistance();
+
+    return nextPos;
+}
+
+
+std::pair<int, int> Wall::handleEdge(std::pair<int, int> curPos, Direction &direction, int colorVal) {
+    std::pair<int, int> nextPos;
+
+    // if ant not at the edge move there first
+    if(std::min(curPos.first, curPos.second) - 1 >= 0 && curPos.first + 1 < LEDBoard::PIXEL_ROWS
+            && curPos.second + 1 < LEDBoard::PIXEL_COLS) {
+        nextPos = adjustPos(moveBehavior->move(curPos, direction));
+    } else {
+        nextPos = curPos;
+
+        // while loop to prevent getting stuck in the corners
+        while (isEdge(curPos, direction)) {
+            direction = turnBehavior->turn(colorVal, direction);
+        }
+    }
+    return nextPos;
+}
+
+RandomTeleport::RandomTeleport(int antSize)
+    : rowRange(0, LEDBoard::PIXEL_ROWS - antSize), colRange(LEDBoard::PIXEL_COLS - antSize) {}
+
+std::pair<int, int> RandomTeleport::handleEdge(std::pair<int, int> curPos, Direction &direction, int colorVal) {
+    return std::make_pair(rowRange(randGen), colRange(randGen));
+}
+
+
--- models/simulations/langtons_ant/EdgeBehavior.hpp
+++ models/simulations/langtons_ant/EdgeBehavior.hpp
@@ -6,9 +6,59 @@
 #define LED_BOARD_PROGRAM_EDGEBEHAVIOR_HPP
 
 
-class EdgeBehavior {
+#include "TurnBehavior.hpp"
+#include "MoveBehavior.hpp"
+#include <board_constants.hpp>
+#include <algorithm>
+#include <random>
 
+class EdgeBehavior {
+protected:
+    MoveBehavior *moveBehavior = nullptr;
+    TurnBehavior *turnBehavior = nullptr;
+
+    std::pair<int, int> adjustPos(std::pair<int, int> nextPos);
+public:
+    bool isEdge(std::pair<int, int> curPos, Direction direction);
+    virtual std::pair<int, int> handleEdge(std::pair<int, int> curPos,
+                                           Direction &direction, int colorVal) = 0;
+    void setMoveBehavior(MoveBehavior *moveBx);
+    void setTurnBehavior(TurnBehavior *turnBx);
 };
 
+class Teleport : public EdgeBehavior {
+public:
+    std::pair<int, int> handleEdge(std::pair<int, int> curPos, Direction &direction, int colorVal) override;
+};
+
+class UTurn : public EdgeBehavior {
+public:
+    std::pair<int, int> handleEdge(std::pair<int, int> curPos, Direction &direction, int colorVal) override;
+};
+
+class Death : public EdgeBehavior {
+private:
+    bool removeOnDeath;
+
+public:
+    explicit Death(bool remove = false);
+    std::pair<int, int> handleEdge(std::pair<int, int> curPos, Direction &direction, int colorVal) override;
+};
+
+class Wall : public EdgeBehavior {
+public:
+    std::pair<int, int> handleEdge(std::pair<int, int> curPos, Direction &direction, int colorVal) override;
+};
+
+class RandomTeleport : EdgeBehavior {
+private:
+    std::default_random_engine randGen;
+    std::uniform_int_distribution<int> rowRange;
+    std::uniform_int_distribution<int> colRange;
+
+public:
+    explicit RandomTeleport(int antSize = 1);
+    std::pair<int, int> handleEdge(std::pair<int, int> curPos, Direction &direction, int colorVal) override;
+};
 
 #endif //LED_BOARD_PROGRAM_EDGEBEHAVIOR_HPP
